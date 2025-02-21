0add594b833d65997376eda7857d9665e02c0246
Bagaev Dmitry <bvdmitri@gmail.com>
2025-02-21 18:20:58 +0100

    update asset prefix

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,16 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
   images: {
     unoptimized: true,
   },
+  assetPrefix: isProd ? 'https://rxinfer.ml' : '',
+  basePath: '',
+  trailingSlash: true,
 };
 
 export default nextConfig;
